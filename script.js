c9cef18820115c101da954a56afdc7f0af107fef
amitnahimov <bjhnuc_100@walla.com>
2015-01-19 22:25:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function answer()
+{
+	var val = document.getElementById('text').value;
+	if(val == ""){
+		alert("ask a question!");
+	}else{
+    	x = Math.floor((Math.random() * 2) + 1);
+		if(x == 1)
+			{
+				alert(val+": yes");
+				document.getElementById('q').innerHTML = document.getElementById('q').innerHTML +"<p>"+val+": yes</p>";
+			} 
+		else 
+			{
+				alert(val+": no");
+				document.getElementById('q').innerHTML = document.getElementById('q').innerHTML +"<p>"+val+": no</p>";
+			}
+	}
+	document.getElementById('text').value = "";
+}
