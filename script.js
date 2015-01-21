cc9175a38ef71b2eae7213e1ee92cb43268697a9
amitnahimov <bjhnuc_100@walla.com>
2015-01-21 09:11:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function answer()
 {
 	var val = document.getElementById('text').value;
-	if(val == "אופק זונה?"){ alert("yes");}
 	if(val == ""){
 		alert("ask a question!");
 	}else{
     	x = Math.floor((Math.random() * 2) + 1);
 		if(x == 1)
-			{
-				alert(val+": yes");
-				document.getElementById('q').innerHTML = "<p>"+val+": yes</p>"+document.getElementById('q').innerHTML;
-			} 
+		{
+		alert(val+": yes");
+		document.getElementById('q').innerHTML = "<p>"+val+": yes</p>"+document.getElementById('q').innerHTML;
+		} 
 		else 
-			{
-				alert(val+": no");
-				document.getElementById('q').innerHTML = "<p>"+val+": no</p>"+document.getElementById('q').innerHTML;
-			}
+		{
+		alert(val+": no");
+		document.getElementById('q').innerHTML = "<p>"+val+": no</p>"+document.getElementById('q').innerHTML;
+		}
 	}
 	document.getElementById('text').value = "";
 }
