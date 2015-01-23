53b705bb5434d88d0d43b526ece6634ac752e3e0
amitnahimov <bjhnuc_100@walla.com>
2015-01-23 09:38:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+function load()
+{
+	$("body").fadeOut("500");
+	$("body").fadeIn("500");
+}
 function answer()
 {
 	var val = document.getElementById('text').value;
+	if(val == "אופק זונה?"){ alert("yes");}
 	if(val == ""){
 		alert("ask a question!");
 	}else{
     	x = Math.floor((Math.random() * 2) + 1);
 		if(x == 1)
-		{
-		alert(val+": yes");
-		document.getElementById('q').innerHTML = "<p>"+val+": yes</p>"+document.getElementById('q').innerHTML;
-		} 
+			{
+				alert(val+": yes");
+				$("#q").html("<p>"+val+": yes</p>"+$("#q").html());
+			} 
 		else 
-		{
-		alert(val+": no");
-		document.getElementById('q').innerHTML = "<p>"+val+": no</p>"+document.getElementById('q').innerHTML;
-		}
+			{
+				alert(val+": no");
+				$("#q").html("<p>"+val+": no</p>"+$("#q").html());
+			}
 	}
+	$("#q").fadeOut("500");
+	$("#q").fadeIn("500");
 	document.getElementById('text').value = "";
 }
